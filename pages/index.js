2bb6749e672d0c5d8ad2f8ec2975a617344a5589
Omi-joshi007 <joshiomkar869@gmail.com>
2025-05-25 12:08:30 +1200

    Portfolio Changes

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
     <Layout>
       <div className={styles.hero}>
         <Image
-          src="/Omkar Photo.jpg"
+          src="/Omkar_Photo.jpg"
           alt="My Profile"
           width={450}
           height={450}
